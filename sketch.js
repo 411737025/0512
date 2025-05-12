ffeca30cf1846834a84eddf3745b1d19d9f81128
411737025 <112852708+411737025@users.noreply.github.com>
2025-05-12 10:53:11 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,19 @@
 let facemesh;
 let video;
 let predictions = [];
-const pointIndices = [
+
+// 定義三組點的編號
+const pointIndicesGroup1 = [
   409, 270, 269, 267, 0, 37, 39, 40, 185, 61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291,
   76, 77, 90, 180, 85, 16, 315, 404, 320, 307, 306, 408, 304, 303, 302, 11, 72, 73, 74, 184
+];
+const pointIndicesGroup2 = [
+  243, 190, 56, 28, 27, 29, 30, 247, 130, 25, 110, 24, 23, 22, 26, 112,
+  133, 173, 157, 158, 159, 160, 161, 246, 33, 7, 163, 144, 145, 153, 154, 155
+];
+const pointIndicesGroup3 = [
+  359, 467, 260, 259, 257, 258, 286, 444, 463, 341, 256, 252, 253, 254, 339, 255,
+  263, 466, 388, 387, 386, 385, 384, 398, 362, 382, 381, 380, 374, 373, 390, 249
 ];
 
 function setup() {
@@ -36,10 +46,12 @@ function draw() {
   image(video, 0, 0, width, height);
 
   // 繪製臉部特徵點的連線
-  drawConnections();
+  drawConnections(pointIndicesGroup1);
+  drawConnections(pointIndicesGroup2);
+  drawConnections(pointIndicesGroup3);
 }
 
-function drawConnections() {
+function drawConnections(pointIndices) {
   if (predictions.length > 0) {
     const keypoints = predictions[0].scaledMesh;
 
